20098bc7a908d5e97ddde4f45fc325e20e1903a8
elp-kkh-at-347123588658 <geunho.kim@neat-soft.com>
2021-09-10 16:54:27 +0900

    google naver 로그인 추가

--- src/main/java/com/exam/webservice/springboot/web/IndexController.java
+++ src/main/java/com/exam/webservice/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.exam.webservice.springboot.web;
 
+import com.exam.webservice.springboot.config.auth.LoginUser;
+import com.exam.webservice.springboot.config.auth.dto.SessionUser;
 import com.exam.webservice.springboot.service.posts.PostService;
 import com.exam.webservice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostService postService;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postService.findAllDesc());
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
